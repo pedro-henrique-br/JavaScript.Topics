f86716ec72a31957e8edea902dc7862a9e836c1f
Pedro Henrique <doctorfast10.12@hotmail.com>
2023-02-04 20:15:04 -0300

    undefined

--- 01-Start-With-These-(Begginer)/01-Primitive-Data-Types/01-string/script.js
+++ 01-Start-With-These-(Begginer)/01-Primitive-Data-Types/01-string/script.js
@@ -7,13 +7,27 @@ const message = "Hello World";
 
 //Object String
  
-const otherMessage = new String (Good Morning World)
+const otherMessage = new String ("Good Morning World")
 
-// Both of the types are a object
-// Both of the types have properties of a object
+// The message is a string
+// And the otherMessage is a object
+// But, both of the types have object properties
+
+console.log(typeof otherMessage)
+console.log(typeof message)
+
+
+// length is the property
 
 console.log(message.length)
 console.log(otherMessage.length)
 
+// Length return the size of characters of the value
 
 
+// ToLocaleUpperCase is the Method
+
+console.log(message.toLocaleUpperCase())
+console.log(otherMessage.toLocaleUpperCase())
+
+// ToLocaleUpperCase return a string with the characters in UpperCase
