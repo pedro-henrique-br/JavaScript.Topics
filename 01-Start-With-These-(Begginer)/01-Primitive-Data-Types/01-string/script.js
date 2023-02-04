1fcb7c2b4a5c3e56c8649db87ffa97791ff64039
Pedro Henrique <doctorfast10.12@hotmail.com>
2023-02-04 20:02:59 -0300

    String

---
+++ 01-Start-With-These-(Begginer)/01-Primitive-Data-Types/01-string/script.js
@@ -0,0 +1,19 @@
+// String is a sequence of characters
+// Used to form a "text" 
+
+//Primitive String
+
+const message = "Hello World";
+
+//Object String
+ 
+const otherMessage = new String (Good Morning World)
+
+// Both of the types are a object
+// Both of the types have properties of a object
+
+console.log(message.length)
+console.log(otherMessage.length)
+
+
+
