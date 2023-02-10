8e3382b768dbb37cfdb62caf5ad4fc5b16818605
Pedro Henrique <71238431+pedro-henrique-br@users.noreply.github.com>
2023-02-10 00:55:25 -0300

    arrow functions

---
+++ 01-Start-With-These-(Begginer)/07-Expressions-And-Operators/02-Arrow-Functions/script.js
@@ -0,0 +1,15 @@
+const concDataType = (string) => {
+  let nome = string
+  if(nome === "string"){
+    console.log(nome + "2")
+  }
+  return string
+}
+
+console.log(concDataType("21"))
+
+const imprimConsole = () => {
+  console.log("Hello World")
+}
+
+imprimConsole()
