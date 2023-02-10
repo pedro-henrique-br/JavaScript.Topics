3570657aa5b81663b5b040fd9d88b57daecfdf75
Pedro Henrique <71238431+pedro-henrique-br@users.noreply.github.com>
2023-02-09 21:22:11 -0300

    bigInt

--- 01-Start-With-These-(Begginer)/01-Primitive-Data-Types/06-Symbol/script.js
+++ 01-Start-With-These-(Begginer)/01-Primitive-Data-Types/06-Symbol/script.js
@@ -1,1 +1,29 @@
-BigInt(value);
+//                Symbol sempre representara um valor novo e unico                                                                        //
+//                Symbol não é uma string, symbol é usado como um identificador interno                                                                                                 //
+
+
+//            A função Symbol() criará um símbolo cujo valor permanece único durante toda a vida
+                                        //útil do programa
+
+let too = Symbol()
+let loo = Symbol()
+
+console.log(too)
+console.log(loo)
+
+console.log(too == loo)
+
+
+
+
+//        métodos "Symbol.for()" e "Symbol.keyFor()" para definir e recuperar símbolos do registro global de
+                                         //symbols
+let oneSymbol = Symbol.for("name")
+let TwoSymbol = Symbol.for("name")
+
+console.log(oneSymbol == TwoSymbol)
+
+console.log(Symbol.keyFor(TwoSymbol) == "name")
+
+
+
--- 01-Start-With-These-(Begginer)/01-Primitive-Data-Types/07-bigInt/script.js
+++ 01-Start-With-These-(Begginer)/01-Primitive-Data-Types/07-bigInt/script.js
@@ -1,1 +1,26 @@
-BigInt(value);
+//                        O bigInt representa valores numericos que são muito grandes
+//                              para serem representados em um number(primitivo)
+
+const bigNumb = BigInt(1312312312115666)
+
+console.log(bigNumb)
+
+let newNumb = 131231231211566613123123121156661312312312115666 
+//essa variavél em tipo number sera representada como(1.3123123121156661 e + 47 ao quadrado)
+
+console.log(newNumb)
+
+
+
+//Ja aqui usando o tipo primitivo bigInt nos teremos o valor completo
+//Que cria e anexa um n no final do literal inteiro
+newNumb = BigInt(131231231211566613123123121156661312312312115666)
+
+console.log(newNumb)
+
+
+
+
+const otherNumber = 3132919239911834194128193121389123n
+
+console.log(otherNumber)//Anexando um n no final de um "number" nos criamos um valor bigInt
