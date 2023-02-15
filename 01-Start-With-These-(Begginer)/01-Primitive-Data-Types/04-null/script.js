410fdb17be425f9dcebbc8a8a6e34c85b488e834
Pedro Henrique <71238431+pedro-henrique-br@users.noreply.github.com>
2023-02-15 00:11:20 -0300

    Number

--- 01-Start-With-These-(Begginer)/01-Primitive-Data-Types/04-null/script.js
+++ 01-Start-With-These-(Begginer)/01-Primitive-Data-Types/04-null/script.js
@@ -1,1 +1,7 @@
-BigInt(value);
+//                      Null é simplesmente a ausencia de um valor de uma determinada propriedade
+
+let empty = null;
+
+console.log(empty + 2, empty, empty - 2, empty)
+
+console.log(!!null) //False
